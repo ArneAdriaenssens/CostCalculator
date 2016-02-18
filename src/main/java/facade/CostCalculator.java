9bbcfc50c51a113b6df9e9a7f4ebf961dfa1aa86
Arne <arne.adriaenssens@hotmail.com>
2016-02-18 13:31:35 +0100

    refactoring

--- src/main/java/facade/CostCalculator.java
+++ src/main/java/facade/CostCalculator.java
@@ -1,13 +1,20 @@
 package facade;
 
+import cost.Cost;
 import cost.repository.CostRepository;
 import factory.CostRepositoryFactory;
 import factory.RepositoryTypes;
 import factory.UserRepositoryFactory;
+import user.User;
+import user.repository.DbUserException;
 import user.repository.UserRepository;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
 /**
- * Created by Arne on 17/02/2016.
+ * Created by Arne on 11/02/2016.
  */
 public class CostCalculator {
 
@@ -37,5 +44,30 @@ public class CostCalculator {
 
     public UserRepository getUserRepository() {
         return userRepository;
+    }
+
+    public List<Cost> getAllCosts(){
+        return this.getCostRepository().getAllCosts();
+    }
+    public void addCost(Cost cost){
+        this.getCostRepository().addCost(cost);
+    }
+    public void deleteCost(Cost cost){
+        this.getCostRepository().deleteCost(cost);
+    }
+
+    public List<User> getAllUsers() throws DbUserException, UnsupportedEncodingException, NoSuchAlgorithmException{
+        return this.getUserRepository().getAllUsers();
+    }
+
+    public User getUserByEmail(String email) throws UnsupportedEncodingException, NoSuchAlgorithmException, DbUserException{
+        return this.getUserRepository().getUserByEmail(email);
+    }
+    public void addUser(User user) throws DbUserException, UnsupportedEncodingException, NoSuchAlgorithmException{
+        this.getUserRepository().addUser(user);
+    }
+
+    public void deleteUser(String email) throws DbUserException{
+        this.getUserRepository().deleteUser(email);
     }
 }
