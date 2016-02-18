bacb5b2551d12aa208db56b4ad8d2f6f93799370
Arne <arne.adriaenssens@hotmail.com>
2016-02-18 21:13:18 +0100

    refactoring

--- src/test/java/CostCalculatorTest.java
+++ src/test/java/CostCalculatorTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+
 /**
  * Created by Arne on 17/02/2016.
  */
