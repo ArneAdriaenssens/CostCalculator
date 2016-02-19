2e24dc1685a5d5456c7714ce977ab7cad53eb35d
Arne <arne.adriaenssens@hotmail.com>
2016-02-19 02:02:24 +0100

    refactoring

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,5 +1,3 @@
-import cost.Cost;
-import cost.FreeTimeCost;
 import facade.CostCalculator;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,12 +5,13 @@ import user.User;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
- * Created by Arne on 17/02/2016.
+ * Created by Arne on 19/02/2016.
  */
-public class CostCalculatorTest {
+public class UserTest {
 
     private CostCalculator facade;
 
@@ -22,19 +21,8 @@ public class CostCalculatorTest {
     }
 
     @Test
-    public void test_creatie_facade_maakt_costrepository(){
-        assertTrue(facade.getCostRepository()!=null);
-    }
-
-    @Test
     public void test_creatie_facade_maakt_userrepository(){
         assertTrue(facade.getUserRepository()!=null);
-    }
-
-    @Test
-    public void test_get_all_cost_geeft_correcte_lijst(){
-        List<Cost> costs = facade.getAllCosts();
-        assertTrue(costs!=null);
     }
 
     @Test
@@ -43,25 +31,6 @@ public class CostCalculatorTest {
         facade.addUser(owner);
         List<User> users = facade.getAllUsers();
         assertTrue(users.size()==1);
-    }
-
-    @Test
-    public void test_add_cost_voegt_geldige_cost_toe(){
-        User owner=new User("Arne", "Adriaenssens", "arne.adriaenssens@email.be", "123");
-        Cost cost = new FreeTimeCost(15.0, owner, "Leuven" );
-        facade.addCost(cost);
-        assertTrue(facade.getCostRepository().getAllCosts().contains(cost));
-    }
-
-    @Test
-    public void test_delete_cost_verwijdert_cost(){
-        User owner=new User("Arne", "Adriaenssens", "arne.adriaenssens@email.be", "123");
-        Cost cost = new FreeTimeCost(15.0, owner, "Leuven" );
-        facade.addCost(cost);
-        System.out.println(facade.getAllCosts().toString());
-        facade.deleteCost(cost);
-        System.out.println(facade.getAllCosts().toString());
-        assertFalse(facade.getCostRepository().getAllCosts().contains(cost));
     }
 
     @Test
