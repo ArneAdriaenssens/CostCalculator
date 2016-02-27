8827ccda491f2c484ca9b80a33de45186eb009e0
ArneAdriaenssens <arne.adriaenssens@hotmail.com>
2016-02-27 14:59:53 +0100

    refactoring

--- src/test/java/facade/CostCalculatorFacadeTest.java
+++ src/test/java/facade/CostCalculatorFacadeTest.java
@@ -6,9 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import user.domain.User;
 
-import java.security.SecureRandom;
 import java.util.List;
-import java.util.Random;
 
 import static org.junit.Assert.*;
 
